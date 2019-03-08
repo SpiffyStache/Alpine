05448cf8ca4dc4b7d0b9088348a660ae48942de1
SpiffyStache <cravens.robbie@gmail.com>
2019-03-08 13:12:33 -0500

    fixed server name

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/alpine-app'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/alpine-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-//test
 app.listen(process.env.PORT || 8080);
