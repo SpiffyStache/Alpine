cfb4a700e32db3fbbb712f8ce8539d0946302b94
SpiffyStache <cravens.robbie@gmail.com>
2019-03-08 12:23:07 -0500

    fixed formatting in server.ts

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,5 +1,5 @@
 
-//Install express server
+// Install express server
 const express = require('express');
 const path = require('path');
 
@@ -8,9 +8,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
